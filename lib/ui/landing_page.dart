d4154198c402f009fe3d50d4ac9e5181ced7b2a4
Saarthak Seth <saarthakseth@gmail.com>
2022-07-26 17:47:20 +0530

    landing page ui implemented

--- lib/ui/landing_page.dart
+++ lib/ui/landing_page.dart
@@ -1,19 +1,27 @@
+import 'dart:io';
+
 import 'package:flutter/material.dart';
+import 'package:flutter_riverpod/flutter_riverpod.dart';
 import 'package:flutter_svg/flutter_svg.dart';
 import 'package:paintroid/ui/pocket_paint.dart';
 
+import '../io/src/entity/image_location.dart';
+import '../io/src/ui/load_image_dialog.dart';
 import 'color_schemes.dart';
+import 'io_handler.dart';
 
-class LandingPage extends StatelessWidget {
+class LandingPage extends ConsumerWidget {
   const LandingPage({Key? key}) : super(key: key);
 
   @override
-  Widget build(BuildContext context) {
+  Widget build(BuildContext context, WidgetRef ref) {
+    final ioHandler = ref.watch(IOHandler.provider);
     final size = MediaQuery.of(context).size;
     return MaterialApp(
       title: 'Pocket Paint',
       theme: ThemeData.from(useMaterial3: true, colorScheme: lightColorScheme),
       home: Scaffold(
+        backgroundColor: lightColorScheme.primary,
         appBar: AppBar(
           title: const Text("Pocket Paint"),
         ),
@@ -21,7 +29,12 @@ class LandingPage extends StatelessWidget {
           height: size.height / 3,
           child: Stack(
             children: [
-              const Placeholder(),
+              Material(
+                child: InkWell(
+                  onTap: () {},
+                  child: const Placeholder(),
+                ),
+              ),
               Center(
                 child: IconButton(
                   iconSize: 264,
@@ -41,14 +54,31 @@ class LandingPage extends StatelessWidget {
           children: [
             FloatingActionButton(
               heroTag: "btn1",
+              backgroundColor: const Color(0xFFFFAB08),
+              foregroundColor: const Color(0xFFFFFFFF),
               child: const Icon(Icons.file_download),
-              onPressed: () {},
+              onPressed: () async {
+                Navigator.of(context).push(
+                  MaterialPageRoute(
+                    builder: (context) => const PocketPaint(),
+                  ),
+                );
+                if (Platform.isIOS) {
+                  final location = await showLoadImageDialog(context);
+                  if (location == null) return;
+                  ioHandler.loadImage(location);
+                } else {
+                  ioHandler.loadImage(ImageLocation.files);
+                }
+              },
             ),
             const SizedBox(
               height: 10,
             ),
             FloatingActionButton(
               heroTag: "btn2",
+              backgroundColor: const Color(0xFFFFAB08),
+              foregroundColor: const Color(0xFFFFFFFF),
               child: const Icon(Icons.add),
               onPressed: () {
                 Navigator.of(context).push(
